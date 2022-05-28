e36b51ac16a1d8eb4a7d7c58888260257f3516bb
Roberto Perez <rob.perez.flores@gmail.com>
2022-05-27 22:30:39 -0700

    Added SDK Injection Functionality that actually works

--- inject.js
+++ inject.js
@@ -1,9 +1,6 @@
-let color = '#3aa757';
 let text = '!'
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-  console.log('Default background color set to %cgreen', `color: ${color}`);
   chrome.action.setBadgeText({ text });
 });
 
--- popup.js
+++ popup.js
@@ -1,37 +1,31 @@
 // Initialize button with user's preferred color
 let submitButton = document.getElementById("submit_button");
 
-// chrome.storage.sync.get("color", ({ color }) => {
-//   changeColor.style.backgroundColor = color;
-// });
 
-// When the button is clicked, inject setPageBackgroundColor into current page
+
+
 submitButton.addEventListener("click", async (e) => {
     e.preventDefault()
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-    let url = document.getElementById("script").value
+    let hotelId = document.getElementById("hotelId").value
+    let apiKey = document.getElementById("apiKey").value
+    let sdkPath = chrome.runtime.getURL('/sdk.min.js')
+    console.log(hotelId, apiKey)
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: loadScript,
-      args: [url]
+      args: [sdkPath, hotelId, apiKey]
     });
   });
-  
-  // The body of this function will be executed as a content script inside the
-  // current page
-  function setPageBackgroundColor() {
-    chrome.storage.sync.get("color", ({ color }) => {
-      document.body.style.backgroundColor = color;
-    });
-  }
 
   // This function will load the script in the current page
-    function loadScript(url) {
+    function loadScript(sdkPath, hotelId, apiKey) {
       return new Promise((resolve, reject) => {
         const script = document.createElement("script");
         script.type = "text/javascript";
-        script.src = url;
+        script.src = sdkPath + '?' + 'hotelId=' + hotelId + '&apiKey=' + apiKey;
         script.id = "selfbook_jssdk"
+        console.log(script)
         script.onload = resolve;
         script.onerror = reject;
         document.body.appendChild(script);
